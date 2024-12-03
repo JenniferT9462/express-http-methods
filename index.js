391b0e196e0b9c5854abef2cafe0accfa8e2336b
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-12-03 10:35:10 -0600

    Added URL parameters and query params

--- index.js
+++ index.js
@@ -21,7 +21,15 @@ app.post('/contact', (req, res) => {
     res.json(data);
     //res.send('This is the Contact Page')
 })
-
+//Route with URL parameters
+app.get('/user/:id', (req, res) => {
+    res.send(req.params)
+})
+//Route with query parameters
+app.get('/search', (req, res) => {
+    const { query, category } = req.query;
+    res.send(`Search Query: ${query}, Category: ${category}`);
+})
 //Catch-all route for undefined routes returns 404
 app.use((req, res, next) => {
     res.status(404).send("Sorry can't find that!")
